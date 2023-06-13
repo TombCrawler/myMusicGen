085366dc372c69b66fbd9d3a837c4af0eeffa4a3
YA <ya@YAs-MacBook-Pro.local>
2023-06-13 19:49:24 +0900

    synthAi.py added which produces a piano roll

--- main.py
+++ main.py
@@ -1,58 +1,34 @@
-import tensorflow as tf
 import numpy as np
-from keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+from music21 import converter, instrument, note, chord, stream, duration
 
+# Load the trained model
+model = load_model('trained_model.h5')
 
-# Define the training data
-training_data = [
-    [60, 62, 64, 65, 67, 69, 71, 72],  # C major scale
-    [60, 62, 63, 65, 67, 68, 70, 72],  # C minor scale
-    [60, 62, 64, 67, 69, 71, 74, 76],  # C major arpeggio
-    [60, 63, 67, 70, 73],  # C5 chord
-    [67, 71, 74, 76, 79],  # G5 chord
-]
+# Set the sequence length for generating new sequences
+sequence_length = 128
 
-# Convert the training data into input-output pairs
-sequences = []
-next_note = []
-for melody in training_data:
-    for i in range(len(melody) - 1):
-        sequences.append(melody[:i+1])
-        next_note.append(melody[i+1])
+# Generate a random seed sequence
+seed = np.random.randint(0, 128, size=(1, sequence_length))
 
-# Pad sequences to ensure equal length
-max_sequence_length = max(map(len, sequences))
-sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=max_sequence_length)
+# Generate notes using the model
+prediction_output = model.predict(seed)
 
-# Convert the sequences and next_note to numpy arrays
-sequences = np.array(sequences)
-next_note = np.array(next_note)
+# Create a MIDI stream
+output_stream = stream.Stream()
+output_stream.append(instrument.Piano())
 
-# Define the model
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Embedding(88, 10, input_length=max_sequence_length),
-    tf.keras.layers.GRU(128, return_sequences=True),
-    tf.keras.layers.GRU(128),
-    tf.keras.layers.Dense(88, activation='softmax')
-])
+# Add notes and chords to the stream
+for pattern in prediction_output:
+    try:
+        # If the pattern is a chord
+        notes = [note.Note(int(n), duration=duration.Duration(0.5)) for n in pattern]
+        chord_obj = chord.Chord(notes)
+        output_stream.append(chord_obj)
+    except:
+        # If not a chord, consider it as a note
+        note_obj = note.Note(int(pattern), duration=duration.Duration(0.5))
+        output_stream.append(note_obj)
 
-# Compile the model
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
-
-# Train the model
-model.fit(sequences, next_note, epochs=100, verbose=2)
-
-# Generate a new melody
-# seed_sequence = [60] 
-# modify the code to ensure that seed_sequence is a 2D array before passing it to the pad_sequences function.
-seed_sequence = np.array([[60]])  # Seed sequence for the generator
-generated_melody = seed_sequence.copy()
-
-for _ in range(10):  # Generate 10 notes
-    input_sequence = sequence.pad_sequences([generated_melody[-1]], maxlen=max_sequence_length, padding='pre')
-    predicted_note = model.predict(input_sequence)[0]
-    generated_melody = np.column_stack((generated_melody, [predicted_note]))
-    seed_sequence = np.column_stack((seed_sequence, [predicted_note]))
-
-# Print the generated melody
-print("Generated Melody:", np.array(generated_melody))
+# Write the stream to a MIDI file
+output_stream.write('midi', fp='path_to_output_midi_file.mid')
---
+++ synthAi.py
@@ -0,0 +1,119 @@
+import collections
+import datetime
+import fluidsynth
+# from mingus.midi import fluidsynth
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+import seaborn as sns
+import tensorflow as tf
+
+from IPython import display
+from matplotlib import pyplot as plt
+from typing import Dict, List, Optional, Sequence, Tuple
+
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+# Sampling rate for audio playback
+_SAMPLING_RATE = 16000
+
+# Download the maestro dataset
+data_dir = pathlib.Path('data/maestro-v2.0.0')
+if not data_dir.exists():
+  tf.keras.utils.get_file(
+      'maestro-v2.0.0-midi.zip',
+      origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
+      extract=True,
+      cache_dir='.', cache_subdir='data',
+  )
+
+# The data above contains 1200+ Midi files
+filenames = glob.glob(str(data_dir/'**/*.mid*'))
+print('Number of files:', len(filenames))
+
+# Process a MIDI file
+sample_file = filenames[1]
+print(sample_file)
+
+# generate a prettyMIDI object for the sample MIDI file
+pm = pretty_midi.PrettyMIDI(sample_file)
+
+# # Play the sample file
+# def display_audio(pm: pretty_midi.PrettyMIDI, seconds=30):
+#   waveform = pm.fluidsynth(fs=_SAMPLING_RATE)
+#   # Take a sample of the generated waveform to mitigate kernel resets
+#   waveform_short = waveform[:seconds*_SAMPLING_RATE]
+#   return display.Audio(waveform_short, rate=_SAMPLING_RATE)
+
+# display_audio(pm)
+
+# type of inst
+print('Number of instruments:', len(pm.instruments))
+instrument = pm.instruments[0]
+instrument_name = pretty_midi.program_to_instrument_name(instrument.program)
+print('Instrument name:', instrument_name)
+
+# Entract notes
+for i, note in enumerate(instrument.notes[:10]):
+  note_name = pretty_midi.note_number_to_name(note.pitch)
+  duration = note.end - note.start
+  print(f'{i}: pitch={note.pitch}, note_name={note_name},'
+        f' duration={duration:.4f}')
+  
+
+
+# Extract notes
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+  pm = pretty_midi.PrettyMIDI(midi_file)
+  instrument = pm.instruments[0]
+  notes = collections.defaultdict(list)
+
+  # Sort the notes by start time
+  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+  prev_start = sorted_notes[0].start
+
+  for note in sorted_notes:
+    start = note.start
+    end = note.end
+    notes['pitch'].append(note.pitch)
+    notes['start'].append(start)
+    notes['end'].append(end)
+    notes['step'].append(start - prev_start)
+    notes['duration'].append(end - start)
+    prev_start = start
+
+  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+raw_notes = midi_to_notes(sample_file)
+raw_notes.head()
+
+
+# convert the pitch values to note names
+get_note_names = np.vectorize(pretty_midi.note_number_to_name)
+sample_note_names = get_note_names(raw_notes['pitch'])
+print(sample_note_names[:10])
+
+
+# Visualize the musical piece
+def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
+  if count:
+    title = f'First {count} notes'
+  else:
+    title = f'Whole track'
+    count = len(notes['pitch'])
+  plt.figure(figsize=(20, 4))
+  plot_pitch = np.stack([notes['pitch'], notes['pitch']], axis=0)
+  plot_start_stop = np.stack([notes['start'], notes['end']], axis=0)
+  plt.plot(
+      plot_start_stop[:, :count], plot_pitch[:, :count], color="b", marker=".")
+  plt.xlabel('Time [s]')
+  plt.ylabel('Pitch')
+  _ = plt.title(title)
+
+# plot_piano_roll(raw_notes, count=100)
+plot_piano_roll(raw_notes)
+plt.show()
